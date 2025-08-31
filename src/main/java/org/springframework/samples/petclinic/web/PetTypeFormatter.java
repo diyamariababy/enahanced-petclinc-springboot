8bde1b8c2f6d2db6ea8bfed3229552514d49566b
diyamariababy <diyamaria97@gmail.com>
2025-08-30 20:03:24 -0400

    Update PetTypeFormatter.java

--- src/main/java/org/springframework/samples/petclinic/web/PetTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetTypeFormatter.java
@@ -15,52 +15,76 @@
  */
 package org.springframework.samples.petclinic.web;
 
-
-import java.text.ParseException;
-import java.util.Collection;
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.Formatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.service.ClinicService;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
- * Instructs Spring MVC on how to parse and print elements of type 'PetType'. Starting from Spring 3.0, Formatters have
- * come as an improvement in comparison to legacy PropertyEditors. See the following links for more details: - The
- * Spring ref doc: http://static.springsource.org/spring/docs/current/spring-framework-reference/html/validation.html#format-Formatter-SPI
- * - A nice blog entry from Gordon Dickens: http://gordondickens.com/wordpress/2010/09/30/using-spring-3-0-custom-type-converter/
- * <p/>
- * Also see how the bean 'conversionService' has been declared inside /WEB-INF/mvc-core-config.xml
- *
- * @author Mark Fisher
- * @author Juergen Hoeller
- * @author Michael Isvy
+ * Test class for {@link PetTypeFormatter}
  */
-public class PetTypeFormatter implements Formatter<PetType> {
+@ExtendWith(MockitoExtension.class)
+class PetTypeFormatterTests {
 
-    private final ClinicService clinicService;
+    @Mock
+    private ClinicService clinicService;
 
+    private PetTypeFormatter petTypeFormatter;
 
-    @Autowired
-    public PetTypeFormatter(ClinicService clinicService) {
-        this.clinicService = clinicService;
+    @BeforeEach
+    void setup() {
+        petTypeFormatter = new PetTypeFormatter(clinicService);
     }
 
-    @Override
-    public String print(PetType petType, Locale locale) {
-        return petType.getName();
+    @Test
+    void testPrint() {
+        PetType petType = new PetType();
+        petType.setName("Hamster");
+        String petTypeName = petTypeFormatter.print(petType, Locale.ENGLISH);
+        assertEquals("Hamster", petTypeName);
     }
 
-    @Override
-    public PetType parse(String text, Locale locale) throws ParseException {
-        Collection<PetType> findPetTypes = this.clinicService.findPetTypes();
-        for (PetType type : findPetTypes) {
-            if (type.getName().equals(text)) {
-                return type;
-            }
-        }
-        throw new ParseException("type not found: " + text, 0);
+    @Test
+    void shouldParse() throws ParseException {
+        Mockito.when(clinicService.findPetTypes()).thenReturn(makePetTypes());
+        PetType petType = petTypeFormatter.parse("Bird", Locale.ENGLISH);
+        assertEquals("Bird", petType.getName());
     }
 
+    @Test
+    void shouldThrowParseException() throws ParseException {
+        Mockito.when(clinicService.findPetTypes()).thenReturn(makePetTypes());
+        Assertions.assertThrows(ParseException.class, () -> petTypeFormatter.parse("Fish", Locale.ENGLISH));
+    }
+
+    /**
+     * Helper method to produce some sample pet types just for test purpose
+     *
+     * @return {@link Collection} of {@link PetType}
+     */
+    private Collection<PetType> makePetTypes() {
+        Collection<PetType> petTypes = new ArrayList<>();
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypes.add(dog);
+
+        PetType bird = new PetType();
+        bird.setName("Bird");
+        petTypes.add(bird);
+
+        return petTypes;
+    }
 }
