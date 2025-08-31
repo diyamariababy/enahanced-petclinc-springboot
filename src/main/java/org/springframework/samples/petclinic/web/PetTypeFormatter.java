ab6d0801c26303b7c630d21b5918c81badb2e7e1
diyamariababy <diyamaria97@gmail.com>
2025-08-30 20:07:13 -0400

    Update PetTypeFormatter.java

--- src/main/java/org/springframework/samples/petclinic/web/PetTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetTypeFormatter.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 package org.springframework.samples.petclinic.web;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,30 +23,25 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.service.ClinicService;
-
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Locale;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
  * Test class for {@link PetTypeFormatter}
+ *
+ * @author Colin But
  */
 @ExtendWith(MockitoExtension.class)
 class PetTypeFormatterTests {
-
     @Mock
     private ClinicService clinicService;
-
     private PetTypeFormatter petTypeFormatter;
-
     @BeforeEach
     void setup() {
         petTypeFormatter = new PetTypeFormatter(clinicService);
     }
-
     @Test
     void testPrint() {
         PetType petType = new PetType();
@@ -55,20 +49,21 @@ class PetTypeFormatterTests {
         String petTypeName = petTypeFormatter.print(petType, Locale.ENGLISH);
         assertEquals("Hamster", petTypeName);
     }
-
     @Test
     void shouldParse() throws ParseException {
         Mockito.when(clinicService.findPetTypes()).thenReturn(makePetTypes());
         PetType petType = petTypeFormatter.parse("Bird", Locale.ENGLISH);
         assertEquals("Bird", petType.getName());
     }
-
     @Test
     void shouldThrowParseException() throws ParseException {
         Mockito.when(clinicService.findPetTypes()).thenReturn(makePetTypes());
-        Assertions.assertThrows(ParseException.class, () -> petTypeFormatter.parse("Fish", Locale.ENGLISH));
-    }
+        Assertions.assertThrows(ParseException.class, () -> {
+Remove useless curly braces around statement (sonar.java.source not set. Assuming 8 or greater.)
 
+            petTypeFormatter.parse("Fish", Locale.ENGLISH);
+        });
+    }
     /**
      * Helper method to produce some sample pet types just for test purpose
      *
@@ -76,15 +71,16 @@ class PetTypeFormatterTests {
      */
     private Collection<PetType> makePetTypes() {
         Collection<PetType> petTypes = new ArrayList<>();
-
-        PetType dog = new PetType();
-        dog.setName("Dog");
-        petTypes.add(dog);
-
-        PetType bird = new PetType();
-        bird.setName("Bird");
-        petTypes.add(bird);
-
+        petTypes.add(new PetType(){
+            {
+                setName("Dog");
+            }
+        });
+        petTypes.add(new PetType(){
+            {
+                setName("Bird");
+            }
+        });
         return petTypes;
     }
 }
